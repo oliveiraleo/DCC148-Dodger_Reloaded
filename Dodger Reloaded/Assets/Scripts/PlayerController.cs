904c8ea387251a91069d4fbd4407d745d566a9a7
Leo <leoferenzini@gmail.com>
2022-01-18 11:28:53 -0300

    Adds the player movements

--- Dodger Reloaded/Assets/Scripts/PlayerController.cs
+++ Dodger Reloaded/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = .1f; // speed of the player
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region Keyboard Input
+        //Player movement on each axis
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= speed;
+            this.transform.position = position;
+        }
+        #endregion
     }
 }
