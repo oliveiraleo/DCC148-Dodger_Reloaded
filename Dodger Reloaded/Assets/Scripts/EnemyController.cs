544adf97314d1a29370f03f4d13060735488b213
Leo <leoferenzini@gmail.com>
2022-01-18 18:19:32 -0300

    Adds more enemy movements

--- Dodger Reloaded/Assets/Scripts/EnemyController.cs
+++ Dodger Reloaded/Assets/Scripts/EnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private int maxMovement = 7; // max movement options of the enemy
     private float speed = .05f; // speed of the enemy
 
     private float horizontalLimits = 8.5f; // horizontal boundaries of the screen
@@ -22,30 +23,84 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Enemy movement on each axis
-        switch (enemyDirection)
+        //Enemy movement control
+        if (this.transform.position.x < -horizontalLimits)
         {
-            case 0:
-                Vector3 position = this.transform.position;
-                position.x -= speed;
-                this.transform.position = position;
-                break;
-            case 1:
-                Vector3 position1 = this.transform.position;
-                position1.x += speed;
-                this.transform.position = position1;
-                break;
-            case 2:
-                Vector3 position2 = this.transform.position;
-                position2.y += speed;
-                this.transform.position = position2;
-                break;
-            case 3:
-                Vector3 position3 = this.transform.position;
-                position3.y -= speed;
-                this.transform.position = position3;
-                break;
+            this.transform.position = new Vector3(-horizontalLimits, this.transform.position.y, 0);
+            enemyDirection = getOtherEnemyDirection(this.enemyDirection);
         }
+        else if (this.transform.position.x > horizontalLimits)
+        {
+            this.transform.position = new Vector3(horizontalLimits, this.transform.position.y, 0);
+            enemyDirection = getOtherEnemyDirection(this.enemyDirection);
+        }
+        //Vertical movement control
+        else if (this.transform.position.y < -verticalLimits)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, -verticalLimits, 0);
+            enemyDirection = getOtherEnemyDirection(this.enemyDirection);
+        }
+        else if (this.transform.position.y > verticalLimits)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, verticalLimits, 0);
+            enemyDirection = getOtherEnemyDirection(this.enemyDirection);
+        }
+        else switch (enemyDirection)
+            {
+                //Enemy movement on each axis
+                case 0:
+                    Vector3 position = this.transform.position;
+                    position.x -= speed;
+                    this.transform.position = position;
+                    break;
+                case 1:
+                    Vector3 position1 = this.transform.position;
+                    position1.x += speed;
+                    this.transform.position = position1;
+                    break;
+                case 2:
+                    Vector3 position2 = this.transform.position;
+                    position2.y += speed;
+                    this.transform.position = position2;
+                    break;
+                case 3:
+                    Vector3 position3 = this.transform.position;
+                    position3.y -= speed;
+                    this.transform.position = position3;
+                    break;
+                case 4:
+                    Vector3 position4 = this.transform.position;
+                    position4.x -= speed;
+                    this.transform.position = position4;
+                    Vector3 position4b = this.transform.position;
+                    position4b.y -= speed;
+                    this.transform.position = position4b;
+                    break;
+                case 5:
+                    Vector3 position5 = this.transform.position;
+                    position5.x += speed;
+                    this.transform.position = position5;
+                    Vector3 position5b = this.transform.position;
+                    position5b.y -= speed;
+                    this.transform.position = position5b;
+                    break;
+                case 6:
+                    Vector3 position6 = this.transform.position;
+                    position6.x += speed;
+                    this.transform.position = position6;
+                    Vector3 position6b = this.transform.position;
+                    position6b.y += speed;
+                    this.transform.position = position6b;
+                    break;
+                case 7:
+                    Vector3 position7 = this.transform.position;
+                    position7.x -= speed;
+                    this.transform.position = position7;
+                    Vector3 position7b = this.transform.position;
+                    position7b.y += speed;
+                    this.transform.position = position7b;
+                    break;
+            }
     }
 
     int getRandomNumber(int min, int max)
@@ -55,7 +110,17 @@ public class EnemyController : MonoBehaviour
 
     int getEnemyDirection()
     {
-        int direction = getRandomNumber(0, 4);
+        int direction = getRandomNumber(0, maxMovement);
         return direction;
+    }
+
+    int getOtherEnemyDirection(int direction)
+    {
+        int otherDirection = getRandomNumber(0, maxMovement);
+        while (otherDirection == direction)
+        {
+            otherDirection = getRandomNumber(0, maxMovement);
+        }
+        return otherDirection;
     }
 }
